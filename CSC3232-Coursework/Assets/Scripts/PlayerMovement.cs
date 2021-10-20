689dd47aec572f324e3fe9f410b18eb8b2f74c3b
Mantas Burcikas <b9037068@newcastle.ac.uk>
2021-10-20 22:34:38 +0100

    Some changes to PlayerMovement script

--- CSC3232-Coursework/Assets/Scripts/PlayerMovement.cs
+++ CSC3232-Coursework/Assets/Scripts/PlayerMovement.cs
@@ -6,23 +6,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5f;
+    [SerializeField] private float jumpForce = 400f;
+    private Rigidbody2D _body;
     private SpriteRenderer _renderer;
     private Animator _animator;
-    private bool grounded;
-    
-    // Start is called before the first frame update
-    void Start()
+    private bool _grounded;
+
+    private void Awake()
     {
-        // Grab references
+        _body = GetComponent<Rigidbody2D>();
         _renderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
-    private void Update()
+    
+    private void FixedUpdate()
     {
-        Rigidbody2D body = GetComponent<Rigidbody2D>();
-        float horizontalInput = Input.GetAxis("Horizontal");
+        var horizontalInput = Input.GetAxis("Horizontal");
         
         // Player flipping
         if (horizontalInput > 0)
@@ -35,28 +34,26 @@ public class PlayerMovement : MonoBehaviour
         }
         
         // Horizontal movement
-        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+        _body.velocity = new Vector2(horizontalInput * speed, _body.velocity.y);
 
         // Vertical movement
-        if (Input.GetKey(KeyCode.Space) && grounded) Jump();
+        if (Input.GetKey(KeyCode.Space) && _grounded) Jump();
 
         // Set Animator parameters
-        _animator.SetBool("Walk", horizontalInput != 0);
-        _animator.SetBool("Grounded", grounded);
+        //_animator.SetBool("Walk", horizontalInput != 0);
+        //_animator.SetBool("Grounded", _grounded);
     }
 
     // Jumping animation
     private void Jump()
     {
-        Rigidbody2D body = GetComponent<Rigidbody2D>();
-        
-        body.velocity = new Vector2(body.velocity.x, speed);
-        _animator.SetTrigger("Jump");
-        grounded = false;
+        _grounded = false;
+        _body.AddForce(new Vector2(0f, jumpForce));
+        //_animator.SetTrigger("Jump");
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Ground")) grounded = true;
+        if (collision.gameObject.CompareTag("Ground")) _grounded = true;
     }
 }
