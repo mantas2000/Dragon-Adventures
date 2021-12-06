091f7ca7a6847fab60082ad465c45d45f1b1d199
Mantas Burcikas <b9037068@newcastle.ac.uk>
2021-12-06 00:13:48 +0000

    Fixed issue with gems bar not displaying. Also, implemented moving bridge to Level 4.

---
+++ CSC3232-Coursework/Assets/Scripts/WaypointMovement.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class WaypointMovement : MonoBehaviour
+{
+    [SerializeField] private Transform[] waypoints;
+    [SerializeField] private float speed = 2f;
+    private int _currentWaypoint;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        // Check if game object reached one of the waypoints
+        if (Vector2.Distance(waypoints[_currentWaypoint].transform.position, transform.position) < .1f)
+        {
+            // Set next waypoint
+            _currentWaypoint = (_currentWaypoint + 1) % waypoints.Length;
+        }
+        
+        // Move towards next waypoint
+        transform.position = Vector2.MoveTowards(transform.position, waypoints[_currentWaypoint].transform.position, speed * Time.deltaTime);
+        
+        // Update AIPath grid during runtime
+        AstarPath.active.Scan();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        // Make game object sticky
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        // Make game object not sticky
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.SetParent(null);
+        }
+    }
+}
