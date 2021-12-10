0c8f368015c6b990059db0498d0ef4e97b32327b
Mantas Burcikas <b9037068@newcastle.ac.uk>
2021-12-10 15:01:19 +0000

    Fixed issue that was causing prefab sprite rotating

--- CSC3232-Coursework/Assets/Scripts/MaintainRotation.cs
+++ CSC3232-Coursework/Assets/Scripts/MaintainRotation.cs
@@ -2,17 +2,9 @@ using UnityEngine;
 
 public class MaintainRotation : MonoBehaviour
 {
-    private Quaternion _rotation;
-
-    // Start is called before the first frame update
-    private void Start()
-    {
-        _rotation = transform.rotation;
-    }
-
     private void LateUpdate()
     {
-        // Return to initial rotation
-        transform.rotation = _rotation;
+        // Maintain initial rotation
+        transform.rotation = Quaternion.identity;
     }
 }
