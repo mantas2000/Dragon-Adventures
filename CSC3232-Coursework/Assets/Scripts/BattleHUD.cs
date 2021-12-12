1b0651561840260ec32ff3208e983d75b45fa0ab
Mantas Burcikas <b9037068@newcastle.ac.uk>
2021-12-12 20:24:13 +0000

    Added fighters names back

--- CSC3232-Coursework/Assets/Scripts/BattleHUD.cs
+++ CSC3232-Coursework/Assets/Scripts/BattleHUD.cs
@@ -4,10 +4,12 @@ using UnityEngine.UI;
 
 public class BattleHUD : MonoBehaviour
 {
+	[SerializeField] private TextMeshProUGUI nameText;
 	[SerializeField] private Slider hpSlider;
 
 	public void SetHUD(FighterInfo fighterInfo)
 	{
+		nameText.text = fighterInfo.fighterName;
 		hpSlider.maxValue = fighterInfo.maxHP;
 		hpSlider.value = fighterInfo.currentHP;
 	}
