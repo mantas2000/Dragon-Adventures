641b6197bc96c46fc22a8986d09f5dc7f138ec84
Mantas Burcikas <b9037068@newcastle.ac.uk>
2021-12-13 01:54:27 +0000

    Updated FighterInfo class

--- CSC3232-Coursework/Assets/Scripts/FighterInfo.cs
+++ CSC3232-Coursework/Assets/Scripts/FighterInfo.cs
@@ -2,26 +2,8 @@
 
 public class FighterInfo : MonoBehaviour
 {
-
+	// Fighter's info
 	public string fighterName;
 	public int maxHP;
 	public int currentHP;
-
-	public bool TakeDamage(int amount)
-	{
-		// Take damage
-		currentHP -= amount;
-
-		return currentHP <= 0;
-	}
-
-	public void Heal(int amount)
-	{
-		// Increase HP
-		currentHP += amount;
-		
-		// Ensure current HP doesn't exceed limits
-		if (currentHP > maxHP) currentHP = maxHP;
-	}
-
 }
